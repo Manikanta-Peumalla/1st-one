60c18e8a3f74392d0a78d26215e1f4c2c8153d36
mani <mani.perumalla2001@gmail.com>
2020-11-03 19:12:39 +0530

    binary search added

--- mani.py
+++ mani.py
@@ -1,6 +1,23 @@
-print("ading of two numbers")
-a=8
-b=8
-c=a+b+True
-print("result:",c)
-print("commit added")
+list = [123,34,455,6,889,78,46,89,90]
+list.sort()
+n=int(input("enter a number"))
+pos=-1
+def bs(li,n):
+    l=0
+    u=len(list)+1
+    while l<=u:
+        mid=(l+u)//2
+        if list[mid]==n:
+            globals() ['pos'] = mid
+            return True
+        else:
+            if list[mid]<n:
+                l=mid+1
+            else:
+                u=mid-1
+    return False
+
+if bs(list,n):
+    print('found at ',pos)
+else:
+    print('not found')
